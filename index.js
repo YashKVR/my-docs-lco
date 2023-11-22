bab5a4ebe13c87ef57c7985bf95711266092a40e
YashKVR <yashincrypto5@gmail.com>
2023-11-22 15:48:13 +0530

    added handling URL Query in swagger

--- index.js
+++ index.js
@@ -62,6 +62,13 @@ app.post('/api/v1/addCourse',(req,res) => {
     res.send(true);
 })
 
+app.get('/api/v1/coursequery',(req,res) => {
+    let location = req.query.location;
+    let device = req.query.device;
+
+    res.send({location, device});
+})
+
 app.listen(4000, () => {
     console.log(`Server is running at PORT: 4000...`);
 })
